8f96f7d68214b3c30cc25e5e58c1d8a081bcb330
pruhsert <pruhsert@servoy.com>
2020-12-18 18:21:55 +0100

    v0.3.11

--- src/svy-toolbar-item-ui.js
+++ src/svy-toolbar-item-ui.js
@@ -11,85 +11,78 @@ export default class SvyToolbarItemUi extends Plugin {
      * @inheritDoc
      */
 	init() {
-		console.log('SvyToolbarItemUi.init() got called');
-
-		this.toolbarItems = [];
-		
-		const editor = this.editor;
-		const itemDefinitions = this.editor.config.get('svyToolbarItem') || [];
-
-		console.log(itemDefinitions);
-
-		itemDefinitions.forEach(
-			definition => this.createToolbarItem(definition)
-		);
+		this.buttons = [];
+		const buttonDefinitions = this.editor.config.get('svyToolbarItem') || [];
+		buttonDefinitions.forEach(definition => this.createToolbarButton(definition));
 
 		this.editor.on('change:isReadOnly', () => {
-			this.toolbarItems.forEach(item => {
-				if (item._syncDisabledState) {
-					item.isEnabled = !this.editor.isReadOnly;
+			this.buttons.forEach(button => {
+				if (button._syncDisabledState) {
+					button.isEnabled = !this.editor.isReadOnly;
 				}
 			});
 		});
 	}
 
     /**
-     * Creates a toolbar item. Clicking this item will execute the callback provided
+     * Creates a toolbar Button. Clicking this button will execute
+     * the specified callback.
      *
-	 * @param {{name:String,label:String,icon:String}} itemConfig the item configuration
+	 * @param buttonDefinition the button configuration
      * @private
      */
-	createToolbarItem(itemConfig) {
+	createToolbarButton(buttonDefinition) {
 		const editor = this.editor;
-		const componentName = itemConfig.name;
 
-		editor.ui.componentFactory.add(componentName, locale => {
-			const view = new ButtonView( locale );
-
-			view.set({
-				label: itemConfig.label,
-				tooltip: true,
-				withText: true
-			});
-
-			if (itemConfig.icon) {
-				view.set('icon', itemConfig.icon);
-			}
-
-			view.isEnabled = true;
-
-			//buttonView._syncDisabledState = typeof itemConfig.syncDisabledState === "boolean" ? itemConfig.syncDisabledState : true;
+		editor.ui.componentFactory.add(buttonDefinition.name, locale => {
+            const button = this.createButton(buttonDefinition.label, buttonDefinition.icon, locale);
+			button.isEnabled = true;
+			button._syncDisabledState = typeof buttonDefinition.syncDisabledState === "boolean" ? buttonDefinition.syncDisabledState : true;
 
 			// Change enabled state and execute the specific callback on click.
-			if (1 === 2) {
-				this.listenTo(view, 'execute', () => {
-					this.setEnabled(view, false);
-					Promise.resolve(itemConfig.onClick(view))
-						.then(() => this.setEnabled(view, true))
-						.catch(() => this.setEnabled(view, true));
-				});
-			}
+			this.listenTo(button, 'execute', () => {
+				this.enableButton(button, false);
+				Promise.resolve(buttonDefinition.onClick(button)).then(() => this.enableButton(button, true)).catch(() => this.enableButton(button, true));
+			});
 
-			this.toolbarItems.push(view);
-
-			console.log(view);
-
-			return view;
+			this.buttons.push(button);
+			return button;
 		});
 	}
 
 	/**
-     * Enables or disables the item according to the button config and the current readOnly-state of the editor.
+     * Enables or disables the button due to the button config
+	 * and the current readOnly-state of the editor.
      *
-	 * @param item the buttonView
-	 * @param {Boolean} enabled new enabled state
+	 * @param button the buttonView
+	 * @param value new enabled state
      * @private
      */
-	setEnabled(item, enabled) {
-		if (item._syncDisabledState) {
-			item.isEnabled = !this.editor.isReadOnly && enabled;
+	enableButton(button, value) {
+		if (button._syncDisabledState) {
+			button.isEnabled = !this.editor.isReadOnly && value;
 		} else {
-			item.isEnabled = enabled;
+			button.isEnabled = value;
 		}
+	}
+
+	/**
+     * Internal creation method of ButtonView objects
+     *
+	 * @param label the button label (string)
+	 * @param icon the button icon (string)
+	 * @param icon the button locale (string)
+     * @private
+     */
+	createButton(label, icon, locale) {
+		const button = new ButtonView(locale);
+
+		button.set({
+			label,
+			icon,
+			tooltip: true
+		});
+
+		return button;
 	}
 }
