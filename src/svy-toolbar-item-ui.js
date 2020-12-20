dc81a238c1ead8bb00cfe8e96d3b06895fb31cb6
pruhsert <pruhsert@servoy.com>
2020-12-20 15:13:09 +0100

    v0.4.1

--- src/svy-toolbar-item-ui.js
+++ src/svy-toolbar-item-ui.js
@@ -19,7 +19,17 @@ export default class SvyToolbarItemUi extends Plugin {
 
 		this.svyToolbarItems = [];
 
-		itemDefinitions.forEach(definition => this.createSvyToolbarItem(definition));
+		itemDefinitions.forEach(definition => 
+			definition.onClick = (buttonView) => console.log(buttonView);
+			this.createSvyToolbarItem(definition);
+		);
+
+		//sync disabled state of editor and toolbar items
+		this.editor.on('change:isReadOnly', () => {
+			this.svyToolbarItems.forEach(button => {
+				button.isEnabled = !this.editor.isReadOnly;
+			});
+		});
 	}
 	
 	createSvyToolbarItem(itemConfig) {
@@ -47,7 +57,16 @@ export default class SvyToolbarItemUi extends Plugin {
             //buttonView.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
 
             // Execute the command when the button is clicked (executed).
-            //this.listenTo( buttonView, 'execute', () => editor.execute( 'insertSimpleBox' ) );
+			//this.listenTo( buttonView, 'execute', () => editor.execute( 'insertSimpleBox' ) );
+
+			// Change enabled state and execute the specific callback on click.
+			this.listenTo(buttonView, 'execute', () => {
+				this.enableButton(buttonView, false);
+				Promise.resolve(itemConfig.onClick(buttonView)).then(() => this.enableButton(buttonView, true)).catch(() => this.enableButton(buttonView, true));
+			});
+			
+			//remember buttonView created to allow to sync disabled state of editor
+			this.svyToolbarItems.push(buttonView);
 
             return buttonView;
         } );
