c561747858222a1165aab4a236e404a1cf7f13d3
pruhsert <pruhsert@servoy.com>
2020-12-20 14:15:41 +0100

    v0.3.15

--- src/svy-toolbar-item-ui.js
+++ src/svy-toolbar-item-ui.js
@@ -11,99 +11,35 @@ export default class SvyToolbarItemUi extends Plugin {
      * @inheritDoc
      */
 	init() {
-		const editor = this.editor;
+        console.log( 'SvyToolbarItemUi#init() got called' );
 
-		editor.ui.componentFactory.add('svyButtonTest', locale => {
-			const view = new ButtonView(locale);
+        const editor = this.editor;
+        const t = editor.t;
 
-			view.set({
-				label: 'This is a test',
-				tooltip: true,
-				withText: true
-			});
+        // The "simpleBox" button must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add( 'simpleBox', locale => {
+            // The state of the button will be bound to the widget command.
+            //const command = editor.commands.get( 'insertSimpleBox' );
 
-			// Callback executed once the icon is clicked
-			view.on('execute', () => {
-				// fire a JS event
-				console.log('button clicked');
-			});
+            // The button will be an instance of ButtonView.
+            const buttonView = new ButtonView( locale );
 
-			return view;
-		});
-		
-		return;
-		this.buttons = [];
-		const buttonDefinitions = this.editor.config.get('svyToolbarItem') || [];
-		buttonDefinitions.forEach(definition => this.createToolbarButton(definition));
+            buttonView.set( {
+                // The t() function helps localize the editor. All strings enclosed in t() can be
+                // translated and change when the language of the editor changes.
+                label: t( 'Simple Box' ),
+                withText: true,
+                tooltip: true
+            } );
 
-		this.editor.on('change:isReadOnly', () => {
-			this.buttons.forEach(button => {
-				if (button._syncDisabledState) {
-					button.isEnabled = !this.editor.isReadOnly;
-				}
-			});
-		});
-	}
+            // Bind the state of the button to the command.
+            //buttonView.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
 
-    /**
-     * Creates a toolbar Button. Clicking this button will execute
-     * the specified callback.
-     *
-	 * @param buttonDefinition the button configuration
-     * @private
-     */
-	createToolbarButton(buttonDefinition) {
-		const editor = this.editor;
+            // Execute the command when the button is clicked (executed).
+            //this.listenTo( buttonView, 'execute', () => editor.execute( 'insertSimpleBox' ) );
 
-		editor.ui.componentFactory.add(buttonDefinition.name, locale => {
-            const button = this.createButton(buttonDefinition.label, buttonDefinition.icon, locale);
-			button.isEnabled = true;
-			button._syncDisabledState = typeof buttonDefinition.syncDisabledState === "boolean" ? buttonDefinition.syncDisabledState : true;
-
-			// Change enabled state and execute the specific callback on click.
-			this.listenTo(button, 'execute', () => {
-				this.enableButton(button, false);
-				Promise.resolve(buttonDefinition.onClick(button)).then(() => this.enableButton(button, true)).catch(() => this.enableButton(button, true));
-			});
-
-			this.buttons.push(button);
-			return button;
-		});
-	}
-
-	/**
-     * Enables or disables the button due to the button config
-	 * and the current readOnly-state of the editor.
-     *
-	 * @param button the buttonView
-	 * @param value new enabled state
-     * @private
-     */
-	enableButton(button, value) {
-		if (button._syncDisabledState) {
-			button.isEnabled = !this.editor.isReadOnly && value;
-		} else {
-			button.isEnabled = value;
-		}
-	}
-
-	/**
-     * Internal creation method of ButtonView objects
-     *
-	 * @param label the button label (string)
-	 * @param icon the button icon (string)
-	 * @param icon the button locale (string)
-     * @private
-     */
-	createButton(label, icon, locale) {
-		const button = new ButtonView(locale);
-
-		button.set({
-			label,
-			icon,
-			tooltip: true
-		});
-
-		return button;
-	}
+            return buttonView;
+        } );
+    }
 }
